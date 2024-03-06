a219d69fc0f837149e851b6684bd953a5961bc3d
bbimber <bbimber@gmail.com>
2024-03-06 13:33:18 -0800

    Allow TRDC

--- R/TCR.R
+++ R/TCR.R
@@ -498,9 +498,10 @@ RunCoNGA <- function(seuratObj,
 #' @param seuratObj The seurat object
 #' @param assayName The name of the RNA assay
 #' @param constantRegionCountThreshold Any cell with a TCR constant region raw count above this threshold is considered positive for that gene
-#' @param includeConstantRegionExpression RNA expression of the constant region genes may be leaky. If includeConstantRegionExpression is true, any cell with expression of any constant region gene above constantRegionCountThreshold is considered positive for TCR expression and therefore not an NK cell.
+#' @param includeConstantRegionExpression If includeConstantRegionExpression is true, RNA expression of TRA/B/G constant regions will be considered in the classification. This is disabled by default because we find expression of these genes may be leaky and not necessarily linked to a functional TCR
+#' @param includeDeltaConstantRegionExpression Similar to includeConstantRegionExpression, but applies to the delta constant region alone
 #' @export
-ClassifyTNKByExpression <- function(seuratObj, assayName = 'RNA', constantRegionCountThreshold = 1.5, includeConstantRegionExpression = FALSE) {
+ClassifyTNKByExpression <- function(seuratObj, assayName = 'RNA', constantRegionCountThreshold = 1.5, includeConstantRegionExpression = FALSE, includeDeltaConstantRegionExpression = TRUE) {
   if (!'HasCDR3Data' %in% names(seuratObj@meta.data)) {
     stop('This seurat object appears to be missing TCR data. See RDiscvr::DownloadAndAppendTcrClonotypes')
   }
@@ -516,7 +517,7 @@ ClassifyTNKByExpression <- function(seuratObj, assayName = 'RNA', constantRegion
   }
 
   # LOC711031 = TRDC
-  seuratObj$IsGammaDelta <- !is.na(seuratObj$TRD) | (includeConstantRegionExpression & testGeneGt0(ad, 'LOC711031'))
+  seuratObj$IsGammaDelta <- !is.na(seuratObj$TRD) | (includeDeltaConstantRegionExpression & testGeneGt0(ad, 'LOC711031'))
   print(DimPlot(seuratObj, group.by = 'IsGammaDelta'))
 
   seuratObj$HasCD3 <- testGeneGt0(ad, 'CD3D') | testGeneGt0(ad, 'CD3E') | testGeneGt0(ad, 'CD3G')
