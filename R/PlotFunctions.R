36e19f8a39fc8e5cc6701ead80ac3860c0617e01
GWMcElfresh <mcelfreshgw@gmail.com>
2025-11-26 09:13:57 -0800

    add docs

--- R/PlotFunctions.R
+++ R/PlotFunctions.R
@@ -4,6 +4,7 @@
 #' @param P1 The plot object
 #' @return A plot object
 #' @export
+
 FormatUmapForPresentation <- function(P1) {
   return(P1 & labs(x = 'UMAP_1', y = 'UMAP_2'))
 }
@@ -14,6 +15,7 @@ FormatUmapForPresentation <- function(P1) {
 #' @param P1 The plot object
 #' @return A plot object
 #' @export
+
 FormatFeaturePlotColorScale <- function(P1) {
   return(FormatUmapForPresentation(P1) + scale_colour_gradientn(colours = c("navy", "dodgerblue", "gold", "red")))
 }
@@ -41,7 +43,6 @@ FormatFeaturePlotColorScale <- function(P1) {
 #'
 #' @return ggplot object
 #' @export
-
 
 theme_bimber <- function(
     standardPlot = TRUE,
@@ -168,7 +169,6 @@ ConvertAxesToArrows <- function(
 #' @method ggplot_add bimber_arrows
 #' @export
 
-
 ggplot_add.bimber_arrows <- function(object, plot, object_name){
   plot <- plot +
     coord_cartesian(clip = "off") +
@@ -217,7 +217,6 @@ ggplot_add.bimber_arrows <- function(object, plot, object_name){
     )
 }
 
-
 #' @title ApplyBimberTheme
 #' 
 #' @description Wrapper for Rdiscvr plotting themes that formats ggplot objects using a standardized theme.
@@ -259,7 +258,6 @@ ggplot_add.bimber_arrows <- function(object, plot, object_name){
 #'   label (set to `90` for vertical).
 #' @return ggplot object
 #' @export
-#' 
 
 ApplyBimberTheme <- function(plot = NULL,
                              standardPlot = TRUE,
