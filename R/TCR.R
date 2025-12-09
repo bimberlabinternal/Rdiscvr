96a556ecc4063e6f673cc5102323024a91d80396
bbimber <bbimber@gmail.com>
2025-12-08 18:13:19 -0800

    Fix warnings

--- R/TCR.R
+++ R/TCR.R
@@ -3,7 +3,7 @@
 #' @import utils
 
 utils::globalVariables(
-  names = c('sortOrder', 'SampleName', 'SubjectId', 'c_gene', 'cdna', 'count', 'd_gene', 'j_gene', 'population', 'raw_clonotype_id', 'raw_consensus_id', 'v_gene', 'CellBarcode', 'IsActive', 'IsActiveLabel', 'GroupField', 'Fraction', 'Label', 'IsShared', 'Tissue'),
+  names = c('sortOrder', 'SampleName', 'SubjectId', 'c_gene', 'cdna', 'count', 'd_gene', 'j_gene', 'population', 'raw_clonotype_id', 'raw_consensus_id', 'v_gene', 'CellBarcode', 'IsActive', 'IsActiveLabel', 'GroupField', 'Fraction', 'Label', 'IsShared', 'Tissue', 'metricName'),
   package = 'Rdiscvr',
   add = TRUE
 )
